695ce9d7dcd012232794964d5f205451e9b7406b
Wolfgang Senfter <w.senfter@gmail.com>
2026-04-27 14:00:27 +0200

    cli: fold cli-defaults into the cli bundle (one default per dir)

--- modules/cli/default.nix
+++ modules/cli/default.nix
@@ -1,8 +1,7 @@
 { config, ... }: {
-  flake.homeManagerModules.cli = {
+  flake.homeManagerModules.cli = { pkgs, ... }: {
     imports = with config.flake.homeManagerModules; [
       bat
-      cli-defaults
       fish
       fzf
       helix
@@ -10,6 +9,32 @@
       starship
       yazi
       zellij
+    ];
+
+    programs = {
+      atuin.enable = true;
+      bottom.enable = true;
+      broot.enable = true;
+      fastfetch.enable = true;
+      fd.enable = true;
+      ripgrep.enable = true;
+      tealdeer.enable = true;
+      zoxide.enable = true;
+    };
+
+    home.packages = with pkgs; [
+      choose
+      comma
+      dust
+      dysk
+      hyperfine
+      jq
+      mcfly
+      nano
+      nanorc
+      procs
+      sd
+      yq-go
     ];
   };
 }
